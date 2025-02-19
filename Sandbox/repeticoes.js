7cf4eace4bdc9f02d3e0d4b3705e5bef03756d1a
kaue godencio <kauegodencio67gmail.com>
2025-02-18 22:01:35 -0300

    ajuste javascript

--- Sandbox/repeticoes.js
+++ Sandbox/repeticoes.js
@@ -21,33 +21,86 @@ while (contador<= 5){
     console.log("Contagem:" + contador);
     contador++;
 } */
-
-
-
-/* 2 Tabuada de um número: */
-/* let num = parseFloat(prompt("Insira um numero: "));
-
-for (let contador = 1; contador <= 10; contador++) {
-
-    let valorCalculado = num * contador;
-    let mensagem = `${num} x ${contador} = ${valorCalculado}`;
-    console.log(mensagem);
-};
- */
-
 /* 
 3. **Soma dos primeiros N números naturais:**
     - Peça um número `N` ao usuário.
     - Use um `while` ou `for` para somar os números de `1` a `N`. */
 
 
-    let n = parseFloat(prompt("Insira um numero: "));
-    let soma = 0
+/*   let n = parseFloat(prompt("Insira um numero: "));
+  let soma = 0
 
-    for( let contador =1 ;contador <= n ; contador++){
-        soma= contador  + soma 
-        console.log(soma)
+  for( let contador =1 ;contador <= n ; contador++){
+      soma= contador  + soma 
+      console.log(soma)
+  }
+*/
+
+// WHILE
+// EXERCICIO 2
+/* let numeroAleatorio = Math.floor(Math.random() * 100);
+
+let chute = null;
+
+while (chute != numeroAleatorio) {
+
+    chute = parseInt(prompt("Chute um número:"));
+
+    if (chute != numeroAleatorio) {
+
+        if (chute > numeroAleatorio) {
+    
+            alert("Chutou alto.");
+    
+        } else {
+    
+            alert("Chutou baixo.");
+    
+        }
+
     }
 
-   
+}
+
+alert("Você acertou!");
+ */
+
+/* 
+3. **Contagem regressiva:**
+    - Peça um número ao usuário e exiba uma contagem regressiva até 0 usando um `while`
+ */
+
+/* Enquanto for verdadeiro */
+
+function exercicioRep01() {
+
+    for (let contador = 1; contador <= 10; contador++) {
+        console.log("contagem :", contador);
+    }
+
+    let divDeLogs = document.querySelector(".logs"); // pega o elemento do HTML
+
+    let paragrafo = document.createElement('p')
+
+    paragrafo.innerHTML = "Exercicio 01 de repetiçao"
+
+    divDeLogs.appendChild(paragrafo);
+
+
+};
+
+function exercicioRep02() {
+    /* 2 Tabuada de um número: */
+    let num = parseFloat(prompt("Insira um numero: "));
+
+    for (let contador = 1; contador <= 10; contador++) {
+
+
+
+        let valorCalculado = num * contador;
+        let mensagem = `${num} x ${contador} = ${valorCalculado}`;
+        console.log(mensagem);
+    };
+
+}
 
